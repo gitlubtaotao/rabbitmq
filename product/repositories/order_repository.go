61b6249ef76333d2d83d150c144fef28340ece33
taotao <oncwnuKJY6G8EpTfb-s3tMZD7Ers@git.weixin.qq.com>
2020-03-08 13:41:12 +0800

    fix

--- product/repositories/order_repository.go
+++ product/repositories/order_repository.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"rabbitmq/product/datamodels"
 	"rabbitmq/product/util"
+	"strconv"
 )
 
 type IOrderRepository interface {
@@ -42,24 +43,102 @@ func (o *OrderMangerRepository) Conn() (err error) {
 }
 
 func (o *OrderMangerRepository) Insert(order *datamodels.Order) (OrderId int64, err error) {
-	return
+	if err = o.Conn(); err != nil {
+		return
+	}
+	s := "INSERT " + o.table + "set userID=?,ProductId=?,OrderStatus=?"
+	stmt, err := o.mysqlConn.Prepare(s)
+	if err != nil {
+		return
+	}
+	defer stmt.Close()
+	result, err := stmt.Exec(order.UserId, order.ProductId, order.OrderStatus)
+	if err != nil {
+		return
+	}
+	return result.LastInsertId()
 }
 
 func (o *OrderMangerRepository) Delete(orderID int64) bool {
+	if err := o.Conn(); err != nil {
+		log.Fatal(err)
+		return false
+	}
+	if stmt, err := o.mysqlConn.Prepare(" DELETE from" + o.table + "WHERE ID= ?"); err != nil {
+		log.Fatal(err)
+		return false
+	} else {
+		defer stmt.Close()
+		_, err := stmt.Exec(orderID)
+		if err != nil {
+			log.Fatal(err)
+			return false
+		}
+	}
 	return true
 }
 
 func (o *OrderMangerRepository) Update(order *datamodels.Order) (err error) {
+	if err = o.Conn(); err != nil {
+		return err
+	}
+	stmt, err := o.mysqlConn.Prepare("UPDATE " + o.table + "SET UserId=?,ProductId=?,OrderStatus=?" + "WHERE ID=" + strconv.FormatInt(order.ID, 10))
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+	defer stmt.Close()
+	_, err = stmt.Exec(order.UserId, order.ProductId, order.OrderStatus)
 	return
 }
 
 func (o *OrderMangerRepository) SelectByKey(orderID int64) (order *datamodels.Order, err error) {
+	order = &datamodels.Order{}
+	if err = o.Conn(); err != nil {
+		return order, err
+	}
+	s := "SELECT * from " + o.table + "WHERE ID=" + strconv.FormatInt(orderID, 10)
+	stmt, err := o.mysqlConn.Prepare(s)
+	if err != nil {
+		return order, err
+	}
+	defer stmt.Close()
+	row, err := stmt.Query(s)
+	if err != nil {
+		return order, err
+	}
+	defer row.Close()
+	result := util.GetResultRow(row)
+	if len(result) == 0 {
+		return order, err
+	}
+	util.DataToStructByTagSql(result, order)
 	return
 }
 
 func (o *OrderMangerRepository) SelectAll() (orders []*datamodels.Order, err error) {
+	if err = o.Conn(); err != nil {
+		return nil, err
+	}
+	s := "Select * from " + o.table
+	rows, err := o.mysqlConn.Query(s)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	
+	result := util.GetResultRows(rows)
+	if len(result) == 0 {
+		return nil, err
+	}
+	for _, v := range result {
+		order := &datamodels.Order{}
+		util.DataToStructByTagSql(v, order)
+		orders = append(orders, order)
+	}
 	return
 }
+
 func (o *OrderMangerRepository) SelectAllWithInfo() (orders map[int]map[string]string, err error) {
 	return
 }
